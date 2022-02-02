e4c41c96fdf2945a071e6b8bb26870acb8e7829e
AlfaRiza <malfariza45@gmail.com>
2022-02-02 15:36:11 +0700

    learn toRefs for get reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- {{ number }} -->
+    <p>{{ user.username }}</p>
+    <p>{{ post.title }}</p>
+    <p>{{ loading }}</p>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+// Ref
+// import { ref } from 'vue'
+// export default {
+//   setup() {
+//     const number = ref(0);
+//     setInterval(() => {
+//       number.value++;
+//     }, 3000);    
+
+//     return {
+//       number
+//     }
+//   }
+// }
+import {reactive, toRefs} from 'vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    // const loading = ref(false)
+    const state = reactive({
+      loading: false,
+      user: {
+        name: "Alfa",
+        username: "masmasakhirzaman"
+      },
+      post: {
+        title: "Title",
+        body: "Body"
+      }
+    });
+
+    setTimeout(() => {
+      state.loading = true
+      // loading.value = true
+      state.user.username = "AlfaRiza"
+      state.post.title = "Title[update]"
+    }, 5000)
+
+    return {
+      ...toRefs(state)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
